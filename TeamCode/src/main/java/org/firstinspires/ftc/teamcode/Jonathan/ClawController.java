4ab7367f43386115c8c22e606c2bf4817b784d17
Jonathan Quapp <jrquapp@gmail.com>
2024-11-20 21:09:29 -0500

    Added claw controller class with methods to open, close and toggle the state of the claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jonathan/ClawController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jonathan/ClawController.java
@@ -1,7 +1,35 @@
 package org.firstinspires.ftc.teamcode.Jonathan;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class ClawController {
-    public void closeClaw() {}
+    private static final double servoOpen = 1;
+    private static final double servoClosed = 0;
+    
+    private Servo servo;
+    
+    public void initializeClaw(@NonNull HardwareMap hardwareMap) {
+        servo = hardwareMap.get(Servo.class, "Claw servo");
+        servo.setDirection(Servo.Direction.FORWARD);
+        servo.setPosition(servoOpen);
+    }
+
+    public void closeClaw() {
+        servo.setPosition(servoClosed);
+    }
+
+    public void openClaw() {
+        servo.setPosition(servoOpen);
+    }
+
+    public void toggleClaw() {
+        if (servo.getPosition() == servoClosed) {
+            servo.setPosition(servoOpen);
+        } else {
+            servo.setPosition(servoClosed);
+        }
+    }
 }
