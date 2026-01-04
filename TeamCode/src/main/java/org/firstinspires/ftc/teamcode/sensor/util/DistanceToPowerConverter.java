b64eda6c01e8181244398aab29bf592a2d3ee45f
Jonathan Quapp <jrquapp@gmail.com>
2026-01-04 16:39:45 -0500

    Added ability to convert camera distance into real distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensor/util/DistanceToPowerConverter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensor/util/DistanceToPowerConverter.java
@@ -4,6 +4,14 @@ public class DistanceToPowerConverter {
 
     public static final double POWER_CONSTANT = 0.3;
 
+    public static final double TO_REAL_DISTANCE_SLOPE = 1.115;
+
+    public static final double TO_REAL_DISTANCE_Y_INTERCEPT = 3.6;
+
+    private static double toRealDistance(double distance) {
+        return TO_REAL_DISTANCE_SLOPE * distance + TO_REAL_DISTANCE_Y_INTERCEPT;
+    }
+
     public static double convert(double distance) {
         return distance * POWER_CONSTANT;
     }
